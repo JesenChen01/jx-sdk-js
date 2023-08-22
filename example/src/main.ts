0a512f60ba71639d18bf728fff44a77110318e6f
Jesen <jesenchen01@gmail.com>
2023-08-22 17:27:56 +0800

    feat: 测试代码

---
+++ example/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { Debounce } from "jx-sdk-js";
+console.log("🚀 ~ file: App.vue:4 ~ jxsdk:", Debounce);
+
+createApp(App).mount("#app");
